0ff44e42b1b0fb1c3045d9474fef39962b43b2dc
DewasSquid <splatsquid4@gmail.com>
2023-07-16 17:32:41 -0300

    settings can now be changed

--- pages/Settings.bat
+++ pages/Settings.bat
@@ -1,12 +1,10 @@
-
-REM TODO: Edit settings
+:settingsPage
 cls
 call modules\navbar "Settings"
 
-REM Display settings
-set /a currentDisplayY=3
+REM Generate a list of settings followed by their states
+set "settingList="
 for /f "tokens=1,2 delims==" %%a in (.\settings.cryuip) do (
-    REM Replace the "_" in the current setting's name by spaces 
     set settingName=%%a
     set settingName=!settingName:_= !
 
@@ -15,19 +13,41 @@ for /f "tokens=1,2 delims==" %%a in (.\settings.cryuip) do (
     set settingState=!settingState:1=ON!
     set settingState=!settingState:0=OFF!
 
-    REM Set the color of the state text according to the current setting's state
-    if !settingState!==ON (
-        set settingStateColor=0x0a
-    ) else (
-        set settingStateColor=0x0c
-    )
+    REM Add spacing after setting name to display setting state
+    call plugins\cmdwiz stringlen "!settingName!"
+    set settingNameLength=!errorlevel!
 
-    REM Display the setting name and the setting state using 2 calls (too long for one line)
-    call plugins\batbox /g 0 !currentDisplayY! /c 0x0f /d "!settingName! "
-    call plugins\batbox /g !windowCenterWidth! !currentDisplayY! /c !settingStateColor! /d "!settingState!"
-    
-    REM Go one line below
-    set /a currentDisplayY=!currentDisplayY! + 1
+    set /a spacingToAdd=!windowCenterWidth! - !settingNameLength!
+    set "settingSpacing="
+    for /l %%i in (0, 1, !spacingToAdd!) do (
+        set "settingSpacing=!settingSpacing! "
+    )
+    set settingDisplay=!settingName!!settingSpacing![!settingState!]
+
+    REM Add new string combining state and name to list
+    set settingList=!settingList! "!settingDisplay!"
 )
 
-pause>nul
+REM Display settings list using cmdmenusel
+call plugins\cmdmenusel 7007 !settingList! "Go back"
+
+REM Get the ID of each settings and modify the state of the selected one according to errorlevel
+set /a settingId=1
+for /f "tokens=1,2 delims==" %%a in (.\settings.cryuip) do (
+    if !settingId!==!errorlevel! (
+        set settingName=%%a
+        set settingState=%%b
+        
+        if !settingState!==1 (
+            set newSettingState=0
+        ) else (
+            set newSettingState=1
+        )
+        
+        call plugins\replaceline "settings.cryuip" !settingId! "!settingName!=!newSettingState!"
+        goto :settingsPage
+    )
+    set /a settingId=!settingId! + 1
+)
+
+call modules\menu.bat
